35478f40e854d0330f19f984527d43f3d720b3cd
James Robson <traptoy@gmail.com>
2017-05-16 20:48:15 -0600

    Adds passwordHandler to consolidate field manipulation

--- content.js
+++ content.js
@@ -14,10 +14,7 @@ for (var i = 0; i < allinputs.length; i++) {
 
 // Selects the appropriate password field and triggers the focus() event on it
 function passwordPickHandler(e) {
-    // NOTE Found in some cases the value change on the password wouldn't
-    // be retained unless it was set at this point, as opposed to the
-    // 'focusin' event above.
-    e.value='wutwut';
+    passwordHandler(e);
 
     // Trigger focus, which seems to be necessary in several key cases in
     // order for styles to get computed, and not be ignored.
@@ -32,9 +29,14 @@ function passwordPickHandler(e) {
 // originally focused element, if any.
 function passwordFocusHandler(e) {
     if (e.target.type && e.target.type === 'password') {
-        e.target.style.backgroundColor = '#FF69B4';
-
+        passwordHandler(e.target);
         // Return focus to the element originally selected by the page
         if (originalFocusElement) originalFocusElement.focus();
     }
+}
+
+
+function passwordHandler(e) {
+    e.value='wutwut';
+    e.style.backgroundColor = '#FF69B4';
 }
